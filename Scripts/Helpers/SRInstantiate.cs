28332b1d3fdb296fa2f9523454e298641ba5a371
Simon Moles <simon.moles@gmail.com>
2014-05-14 18:18:09 +0100

    Added SRInstantiate (typed prefab instantiation)

---
+++ Scripts/Helpers/SRInstantiate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+public static class SRInstantiate
+{
+
+	public static T Instantiate<T>(T prefab) where T : Component
+	{
+		return (T)Object.Instantiate(prefab);
+	}
+	
+	public static T Instantiate<T>(T prefab, Vector3 position, Quaternion rotation) where T : Component
+	{
+		return (T)Object.Instantiate(prefab, position, rotation);
+	}
+
+}
