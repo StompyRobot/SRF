5c3406f9201547fc15635342ee87ae2affe9f947
Simon Moles <simon.moles@gmail.com>
2014-05-15 11:59:54 +0100

    Added PrefabContainer

---
+++ Scripts/Components/PrefabContainer.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using SRF;
+using UnityEngine;
+
+public abstract class PrefabContainer<T> : SRSingleton<PrefabContainer<T>> where T : MonoBehaviour
+{
+
+	public T[] Prefabs;
+	
+#if UNITY_EDITOR
+
+	[ContextMenu("Refresh List")]
+	public void RefreshList()
+	{
+
+		Prefabs = PrefabUtil.FindPrefabs<T>().ToArray();
+
+	}
+
+#endif
+
+}
