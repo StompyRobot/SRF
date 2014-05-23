06c4af3206e5bccc122d49b57d164f6572c51100
Simon Moles <simon.moles@gmail.com>
2014-05-23 17:57:26 +0100

    Added scroll texture behaviour

---
+++ Scripts/Behaviours/ScrollTexture.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace SRF.Behaviours
+{
+
+	public class ScrollTexture : MonoBehaviour
+	{
+
+		public int Material;
+		public string TextureName = "_MainTex";
+
+		public Vector2 ScrollSpeed;
+
+		public bool Shared = true;
+
+		private Renderer _renderer;
+
+		void Update()
+		{
+
+			if (_renderer == null)
+				_renderer = renderer;
+
+			Material m;
+
+			if (Shared)
+				m = _renderer.sharedMaterials[Material];
+			else
+				m = _renderer.materials[Material];
+
+			var dt = Time.time;
+
+			m.SetTextureOffset(TextureName, ScrollSpeed*dt);
+
+		}
+
+	}
+
+}
