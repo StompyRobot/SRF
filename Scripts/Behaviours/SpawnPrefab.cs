541c9a63c5152ece27767cdb817feb673b76f6c1
Simon Moles <simon.moles@gmail.com>
2014-05-23 13:34:16 +0100

    Added SpawnPrefab and MatchOrthoSize scripts

---
+++ Scripts/Behaviours/SpawnPrefab.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Spawn a prefab and add it as a child to this object (useful for nesting prefabs)
+/// </summary>
+public class SpawnPrefab : SRMonoBehaviour
+{
+
+	public GameObject Prefab;
+
+	public bool ResetLocals = true;
+
+	void Start()
+	{
+
+		var o = (GameObject)Instantiate(Prefab, CachedTransform.position, CachedTransform.rotation);
+		o.transform.parent = CachedTransform;
+
+		if (ResetLocals) {
+			o.transform.SetLocals(Prefab.transform);
+		}
+
+		Destroy(this);
+
+	}
+
+}
---
+++ Scripts/Camera/MatchOrthoSize.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace SRF.Camera
+{
+	[RequireComponent(typeof(UnityEngine.Camera))]
+	[ExecuteInEditMode]
+	public class MatchOrthoSize : SRMonoBehaviourEx
+	{
+
+		[RequiredField]
+		public UnityEngine.Camera Target;
+
+		private UnityEngine.Camera _this;
+
+		void LateUpdate()
+		{
+
+			if (_this == null) {
+
+				_this = GetComponent<UnityEngine.Camera>();
+
+			}
+
+			if (_this == null || Target == null)
+				return;
+
+			_this.orthographicSize = Target.orthographicSize;
+
+		}
+
+	}
+}
