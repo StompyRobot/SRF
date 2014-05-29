a4ea11038406ffa0935016115741be3ee2a859b5
Simon Moles <simon.moles@gmail.com>
2014-05-29 15:02:30 +0100

    Hierarchy now uses static Get() method instead of singleton instance indexer.

--- Scripts/Helpers/Hierarchy.cs
+++ Scripts/Helpers/Hierarchy.cs
@@ -7,59 +7,51 @@ using System.Collections;
 public class Hierarchy : SRAutoSingleton<Hierarchy>
 {
 
-//#if UNITY_EDITOR
+	private static readonly char[] Seperator = new[] {'/'};
 
-	private readonly char[] _seperator = new[] {'/'};
+	private static readonly Dictionary<string, Transform> Cache = new Dictionary<string, Transform>();
 
-	private readonly Dictionary<string, Transform> _cache = new Dictionary<string, Transform>();
-
-	public Transform this[string key]
+	public static Transform Get(string key)
 	{
-		get
-		{
 
-			Transform t;
+		Transform t;
 
-			// Check cache
-			if (_cache.TryGetValue(key, out t))
-				return t;
+		// Check cache
+		if (Cache.TryGetValue(key, out t))
+			return t;
 
-			var find = GameObject.Find(key);
+		var find = GameObject.Find(key);
 
-			if (find) {
+		if (find) {
 
-				t = find.transform;
-				_cache.Add(key, t);
-
-				return t;
-
-			}
-
-			// Find container parent
-			var elements = key.Split(_seperator, StringSplitOptions.RemoveEmptyEntries);
-
-			// Create new container
-			t = new GameObject(elements.Last()).transform;
-			_cache.Add(key, t);
-
-			// If root
-			if (elements.Length == 1) 
-				return t;
-
-			t.parent = this[string.Join("/", elements, 0, elements.Length - 1)];
+			t = find.transform;
+			Cache.Add(key, t);
 
 			return t;
 
 		}
+
+		// Find container parent
+		var elements = key.Split(Seperator, StringSplitOptions.RemoveEmptyEntries);
+
+		// Create new container
+		t = new GameObject(elements.Last()).transform;
+		Cache.Add(key, t);
+
+		// If root
+		if (elements.Length == 1)
+			return t;
+
+		t.parent = Get(string.Join("/", elements, 0, elements.Length - 1));
+
+		return t;
+
 	}
 
-/*#else
-
+	[Obsolete("Use static Get() instead")]
 	public Transform this[string key]
 	{
-		get { return null; }
+		get { return Get(key); }
 	}
-
-#endif*/
 
 }
