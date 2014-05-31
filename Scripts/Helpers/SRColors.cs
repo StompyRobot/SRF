de6e9c4713d033b41cf41ecf95db79d846a36495
Simon Moles <simon.moles@gmail.com>
2014-06-01 00:55:03 +0100

    Added Color average method to SRColors

--- Scripts/Helpers/SRColors.cs
+++ Scripts/Helpers/SRColors.cs
@@ -12,4 +12,18 @@ public static class SRColors
 	public static readonly Color Red = new Color32(203, 130, 126, 255);
 	public static readonly Color Yellow = new Color32(201, 201, 126, 255);
 
+
+	public static Color Average(Color c1, Color c2)
+	{
+
+		return new Color(
+			SRMath.Average(c1.r, c2.r),
+			SRMath.Average(c1.g, c2.g),
+			SRMath.Average(c1.b, c2.b),
+			SRMath.Average(c1.a, c2.a)
+			);
+
+	}
+
+
 }
