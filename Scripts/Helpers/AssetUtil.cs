bd7743e310f71b5e901965e0cd94554eed06111f
Simon Moles <simon.moles@gmail.com>
2014-05-31 18:43:44 +0100

    Added Asset util

---
+++ Scripts/Helpers/AssetUtil.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace SRF.Helpers
+{
+	public static class AssetUtil
+	{
+
+#if UNITY_EDITOR
+
+		// Copied from FingerGestures
+
+		/// <summary>
+		//	This makes it easy to create, name and place unique new ScriptableObject asset files.
+		/// </summary>
+		public static T CreateAsset<T>() where T : ScriptableObject
+		{
+			string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+
+			if (path == "")
+				path = "Assets";
+			else if (Path.GetExtension(path) != "")
+				path = path.Replace(Path.GetFileName(path), "");
+
+			return CreateAsset<T>(path, "New " + typeof (T).Name);
+		}
+
+		public static T CreateAsset<T>(string path, string name) where T : ScriptableObject
+		{
+			if (string.IsNullOrEmpty(path))
+				path = "Assets";
+
+			if (!name.EndsWith(".asset"))
+				name += ".asset";
+
+			string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/" + name);
+
+			T asset = ScriptableObject.CreateInstance<T>();
+			AssetDatabase.CreateAsset(asset, assetPathAndName);
+			AssetDatabase.SaveAssets();
+
+			return asset;
+		}
+
+		public static void SelectAssetInProjectView(Object asset)
+		{
+			EditorUtility.FocusProjectWindow();
+			Selection.activeObject = asset;
+		}
+
+#endif
+
+	}
+}
