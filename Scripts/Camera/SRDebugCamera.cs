2209d2c7e21677db3b0656cb437b0d439e2bdfd4
Simon Moles <simon.moles@gmail.com>
2014-07-14 19:01:21 +0100

    Added line drawing functions to SRDebug Added SRDebugCamera for drawing the lines posted to SRDebug

---
+++ Scripts/Camera/SRDebugCamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SRF.Camera
+{
+
+	[RequireComponent(typeof (UnityEngine.Camera))]
+	public class SRDebugCamera : SRMonoBehaviourEx
+	{
+
+		[RequiredField(true)] public UnityEngine.Camera Camera;
+
+		protected override void FixedUpdate()
+		{
+
+			base.FixedUpdate();
+
+			SRDebug.FlushFixedUpdateLines();
+			SRDebug.IsFixedUpdate = true;
+
+		}
+
+		protected override void Update()
+		{
+
+			base.Update();
+
+			SRDebug.FlushLines();
+			SRDebug.IsFixedUpdate = false;
+
+		}
+
+
+		private void OnPostRender()
+		{
+			SRDebug.DrawDebugFrame(Camera);
+		}
+
+	}
+
+}
--- Scripts/Helpers/SRDebug.cs
+++ Scripts/Helpers/SRDebug.cs
@@ -7,6 +7,10 @@ using Debug = UnityEngine.Debug;
 public static class SRDebug
 {
 
+	public static bool IsFixedUpdate { get; set; }
+
+	public const int LineBufferCount = 512;
+
 	[DebuggerNonUserCode]
 	[DebuggerStepThrough]
 	public static void AssertNotNull(object value, string message = null, MonoBehaviour instance = null)
@@ -55,6 +59,107 @@ public static class SRDebug
 	public static void EditorAssert(bool condition, string message = null, MonoBehaviour instance = null)
 	{
 		Assert(condition, message, instance);
+	}
+
+	private struct Line
+	{
+
+		public Vector3 Source;
+		public Vector3 Dest;
+
+		public Color Color;
+
+	}
+
+	private static Line[] _lineBuffer;
+	private static Line[] _fixedUpdateBuffer;
+
+	private static int _currentFrameLines = 0;
+	private static int _currentFixedFrameLines = 0;
+	private static Material _m;
+
+	public static void DrawLine(Vector3 srcPos, Vector3 endPos, Color color)
+	{
+
+		if(_lineBuffer == null)
+			_lineBuffer = new Line[LineBufferCount];
+
+		if(_fixedUpdateBuffer == null)
+			_fixedUpdateBuffer = new Line[LineBufferCount];
+
+		if (_currentFrameLines >= _lineBuffer.Length) {
+			Debug.LogWarning("[SRDebug] Line buffer overflowed");
+			return;
+		}
+
+		var buffer = IsFixedUpdate ? _fixedUpdateBuffer : _lineBuffer;
+
+		var i = IsFixedUpdate ? _currentFixedFrameLines++ : _currentFrameLines++;
+
+		buffer[i].Source = srcPos;
+		buffer[i].Dest = endPos;
+		buffer[i].Color = color;
+
+	}
+
+	public static void DrawDebugFrame(Camera c)
+	{
+
+		if (_fixedUpdateBuffer == null || _lineBuffer == null)
+			return;
+
+		if (_currentFrameLines == 0 && _currentFixedFrameLines == 0)
+			return;
+
+		if (_m == null) {
+			_m = new Material(Shader.Find("GUI/Text Shader"));
+		}
+
+		GL.PushMatrix();
+		_m.SetPass(0);
+		GL.LoadOrtho();
+
+
+		for (var i = 0; i < _currentFrameLines; i++) {
+
+			GL.Begin(GL.LINES);
+
+			GL.Color(_lineBuffer[i].Color);
+			GL.Vertex(c.WorldToViewportPoint(_lineBuffer[i].Source).xy());
+
+			GL.Color(_lineBuffer[i].Color);
+			GL.Vertex(c.WorldToViewportPoint(_lineBuffer[i].Dest).xy());
+
+			GL.End();
+
+		}
+
+		for (var i = 0; i < _currentFixedFrameLines; i++) {
+
+			GL.Begin(GL.LINES);
+
+			GL.Color(_fixedUpdateBuffer[i].Color);
+			GL.Vertex(c.WorldToViewportPoint(_fixedUpdateBuffer[i].Source).xy());
+
+			GL.Color(_fixedUpdateBuffer[i].Color);
+			GL.Vertex(c.WorldToViewportPoint(_fixedUpdateBuffer[i].Dest).xy());
+
+			GL.End();
+
+		}
+
+		GL.PopMatrix();
+
+	}
+
+	public static void FlushLines()
+	{
+		_currentFrameLines = 0;
+	}
+
+	public static void FlushFixedUpdateLines()
+	{
+		_currentFixedFrameLines = 0;
 	}
 
 }
