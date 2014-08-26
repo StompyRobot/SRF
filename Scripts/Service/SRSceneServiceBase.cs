1f231fd9a93943e84dcb8a5d1f1ae265f7955444
Simon Moles <simon@stompyrobot.co.uk>
2014-08-26 18:14:00 +0100

    Fix internal compiler error on unity free.

--- Scripts/Service/SRSceneServiceBase.cs
+++ Scripts/Service/SRSceneServiceBase.cs
@@ -15,12 +15,6 @@ namespace Scripts.Framework.Service
 		private const bool Logging = false;
 #endif
 
-#if UNITY_PRO_LICENSE
-		private const bool IsPro = true;
-#else
-		private const bool IsPro = false;
-#endif
-
 		/// <summary>
 		/// Name of the scene this service's contents are within
 		/// </summary>
@@ -69,10 +63,11 @@ namespace Scripts.Framework.Service
 			if (Logging)
 				Debug.Log("[Service] Loading scene ({0})".Fmt(SceneName), this);
 
-			if(IsPro)
+			#if UNITY_PRO_LICENSE
 				yield return Application.LoadLevelAdditiveAsync(SceneName);
-			else
+			#else
 				Application.LoadLevelAdditive(SceneName);
+			#endif
 
 			if (Logging)
 				Debug.Log("[Service] Scene loaded. Searching for root object...", this);
