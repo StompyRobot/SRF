364de97c6969a6ecf759d4e14f09746626dddd0e
Simon Moles <simon.moles@gmail.com>
2014-08-28 18:34:45 +0100

    Added GameObject overload to SRInstantiate

--- Scripts/Helpers/SRInstantiate.cs
+++ Scripts/Helpers/SRInstantiate.cs
@@ -9,6 +9,11 @@ public static class SRInstantiate
 		return (T)Object.Instantiate(prefab);
 	}
 	
+	public static GameObject Instantiate(GameObject prefab)
+	{
+		return (GameObject)Object.Instantiate(prefab);
+	}
+	
 	public static T Instantiate<T>(T prefab, Vector3 position, Quaternion rotation) where T : Component
 	{
 		return (T)Object.Instantiate(prefab, position, rotation);
