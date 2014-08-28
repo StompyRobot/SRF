f82ae227519e5cef847c1a9c959e78c31c1756bd
Simon Moles <simon.moles@gmail.com>
2014-08-28 20:02:38 +0100

    Added FlashGraphic

---
+++ Scripts/UI/FlashGraphic.cs
@@ -0,0 +1,38 @@
+﻿#if ENABLE_UNITYEVENTS
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace SRF.UI
+{
+	public class FlashGraphic : UIBehaviour, IPointerDownHandler, IPointerUpHandler
+	{
+
+		public Graphic Target;
+
+		public Color DefaultColor = new Color(1,1,1,0);
+		public Color FlashColor = Color.white;
+
+		public float DecayTime = 0.15f;
+
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+			Target.CrossFadeColor(DefaultColor, 0f, true, true);
+		}
+
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			Target.CrossFadeColor(FlashColor, 0f, true, true);
+		}
+
+		public void OnPointerUp(PointerEventData eventData)
+		{
+			Target.CrossFadeColor(DefaultColor, DecayTime, true, true);
+		}
+
+	}
+}
+
+#endif
