45a93dfbc53a536c29363a30e4b6870275c2808f
Simon Moles <simon.moles@gmail.com>
2014-08-29 01:47:58 +0100

    Added experimental Unity UI layout behaviours.

---
+++ Scripts/UI/CopyPreferredSize.cs
@@ -0,0 +1,135 @@
+﻿#if ENABLE_UNITYEVENTS
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace SRF.UI
+{
+
+	[RequireComponent(typeof(RectTransform))]
+	[ExecuteInEditMode]
+	public class CopyPreferredSize : UIBehaviour, ILayoutElement
+	{
+
+		public RectTransform CopySource;
+
+		public float minWidth
+		{
+			get
+			{
+				if (CopySource == null || !IsActive())
+					return -1f;
+				return LayoutUtility.GetMinWidth(CopySource);
+			}
+		}
+
+		public float preferredWidth
+		{
+			get
+			{
+				if (CopySource == null || !IsActive())
+					return -1f;
+				return LayoutUtility.GetPreferredWidth(CopySource);
+			}
+		}
+
+		public float flexibleWidth
+		{
+			get
+			{
+				if (CopySource == null || !IsActive())
+					return -1f;
+				return LayoutUtility.GetFlexibleWidth(CopySource);
+			}
+		}
+
+		public float minHeight
+		{
+			get
+			{
+				if (CopySource == null || !IsActive())
+					return -1f;
+				return LayoutUtility.GetFlexibleHeight(CopySource);
+			}
+		}
+
+		public float preferredHeight
+		{
+			get
+			{
+				if (CopySource == null || !IsActive())
+					return -1f;
+				return LayoutUtility.GetPreferredHeight(CopySource);
+			}
+		}
+
+		public float flexibleHeight
+		{
+			get
+			{
+				if (CopySource == null || !IsActive())
+					return -1f;
+				return LayoutUtility.GetFlexibleHeight(CopySource);
+			}
+		}
+
+		public int layoutPriority
+		{
+			get { return 1; }
+		}
+
+
+		public void CalculateLayoutInputHorizontal()
+		{
+			foreach (var component in CopySource.GetComponents(typeof (ILayoutElement))) {
+				(component as ILayoutElement).CalculateLayoutInputHorizontal();
+			}
+		}
+
+		public void CalculateLayoutInputVertical()
+		{
+			foreach (var component in CopySource.GetComponents(typeof(ILayoutElement))) {
+				(component as ILayoutElement).CalculateLayoutInputVertical();
+			}
+		}
+
+		protected override void OnEnable()
+		{
+			SetDirty();
+		}
+
+		protected override void OnTransformParentChanged()
+		{
+			SetDirty();
+		}
+
+		protected override void OnDisable()
+		{
+			SetDirty();
+		}
+
+		protected override void OnDidApplyAnimationProperties()
+		{
+			SetDirty();
+		}
+
+		protected override void OnBeforeTransformParentChanged()
+		{
+			SetDirty();
+		}
+
+		protected void SetDirty()
+		{
+
+			if (!IsActive())
+				return;
+
+			LayoutRebuilder.MarkLayoutForRebuild(transform as RectTransform);
+
+		}
+
+	}
+}
+
+#endif
---
+++ Scripts/UI/SRText.cs
@@ -0,0 +1,27 @@
+﻿#if ENABLE_UNITYEVENTS
+
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SRF.UI
+{
+	public class SRText : Text
+	{
+
+		public event Action<SRText> LayoutDirty;
+
+		public override void SetLayoutDirty()
+		{
+
+			base.SetLayoutDirty();
+
+			if (LayoutDirty != null)
+				LayoutDirty(this);
+
+		}
+
+	}
+}
+
+#endif
