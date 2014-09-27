44ca10eb329e06ac6a46ec808c07710bf172546d
Simon Moles <simon.moles@gmail.com>
2014-09-28 00:42:09 +0100

    Added Revert Selection To Prefab menu item

---
+++ Scripts/Editor/RevertToPrefab.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SRF.Editor
+{
+	public class RevertToPrefab : ScriptableObject
+	{
+		[MenuItem("Tools/Revert Selected GameObjects to Prefab")]
+		static void DoRevert()
+		{
+			var s = Selection.gameObjects;
+
+			var i = 0;
+
+			foreach (var gameObject in s) {
+
+				if (PrefabUtility.RevertPrefabInstance(gameObject)) {
+
+					++i;
+
+				}
+
+			}
+
+			Debug.Log("Reverted {0} GameObjects to prefab state".Fmt(i));
+
+		}
+
+	}
+}
