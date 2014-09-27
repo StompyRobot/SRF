24698ebdfd0bc90d28a023e8116afe556ffa25df
Simon Moles <simon.moles@gmail.com>
2014-09-27 18:16:50 +0100

    Added RealTime helper class for Unity 4.5+

---
+++ Scripts/Helpers/RealTime.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if !(UNITY_4_0 || UNITY_4_0_1 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3)
+
+public static class RealTime
+{
+
+	public static float deltaTime {get { return Time.unscaledDeltaTime; }}
+	public static float time {get { return Time.unscaledTime; }}
+
+}
+
+#endif
