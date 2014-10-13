9ca45363e27b581d714086574fec5d5ce4d71f09
Simon Moles <simon.moles@gmail.com>
2014-10-13 16:46:19 +0100

    Added PreventHotReload, ends editor play mode if code compile is detected.

---
+++ Scripts/Editor/PreventHotReload.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class PreventHotLoad : EditorWindow
+{
+
+	static PreventHotLoad()
+	{
+		EditorApplication.update += Update;
+	}
+
+	private static void Update()
+	{
+
+		if (EditorApplication.isCompiling && EditorApplication.isPlaying) {
+
+			Debug.Log("[PreventHotReload] Code reload in progress. Ending play mode.");
+			EditorApplication.isPlaying = false;
+
+		}
+
+	}
+
+}
