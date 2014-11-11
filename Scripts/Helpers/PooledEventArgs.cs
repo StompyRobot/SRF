f606ef4b91456d31e2c4518179183fb5d6410ced
Simon Moles <simon.moles@gmail.com>
2014-11-11 00:21:24 +0000

    Added PooledEventArgs base class

---
+++ Scripts/Helpers/PooledEventArgs.cs
@@ -0,0 +1,53 @@
+﻿using Smooth.Pools;
+using UnityEngine;
+using System.Collections;
+
+namespace SRF.Helpers
+{
+
+	public abstract class PooledEventArgs<T> where T : PooledEventArgs<T>, new()
+	{
+
+		protected PooledEventArgs()
+		{
+			
+		}
+
+		/// <summary>
+		/// Override in child classes to reset event args to default values
+		/// </summary>
+		protected abstract void Reset();
+
+		/// <summary>
+		/// Singleton List<T> pool.
+		/// </summary>
+		private static class InternalPool
+		{
+
+			private static readonly Pool<T> _Instance = new Pool<T>(
+				() => new T(),
+				t => t.Reset());
+
+			/// <summary>
+			/// Singleton List<T> pool instance.
+			/// </summary>
+			public static Pool<T> Instance
+			{
+				get { return _Instance; }
+			}
+
+		}
+
+		public static T Borrow()
+		{
+			return InternalPool.Instance.Borrow();
+		}
+
+		public static void Release(T t)
+		{
+			InternalPool.Instance.Release(t);
+		}
+
+	}
+
+}
