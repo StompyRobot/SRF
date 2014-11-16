40d84953b6ace8ff7edddd3e037cf54071eea500
Simon Moles <simon.moles@gmail.com>
2014-11-16 23:41:37 +0000

    Added UI helper methods InheritColour, ScaleOnTouch, TiltOnTouch

---
+++ Scripts/UI/InheritColour.cs
@@ -0,0 +1,53 @@
+﻿#if ENABLE_4_6_FEATURES
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SRF.UI
+{
+	[RequireComponent(typeof(Graphic))]
+	[ExecuteInEditMode]
+	public class InheritColour : SRMonoBehaviour
+	{
+
+		public Graphic From;
+
+		private Graphic Graphic
+		{
+			get
+			{
+
+				if (_graphic == null)
+					_graphic = GetComponent<Graphic>();
+
+				return _graphic;
+
+			}
+		}
+
+		private Graphic _graphic;
+
+		void Refresh()
+		{
+
+			if (From == null)
+				return;
+
+			Graphic.color = From.color;
+
+		}
+
+		void Update()
+		{
+			Refresh();
+		}
+
+		void Start()
+		{
+			Refresh();
+		}
+
+	}
+}
+
+#endif
