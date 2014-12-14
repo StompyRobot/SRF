9bb41bb4ea9c154e043b4ddf034b5f83d7af2353
Simon Moles <simon@stompyrobot.uk>
2014-12-14 23:09:03 +0000

    Added PropertyReference, simple wrapper around PropertyInfo SetValue/GetValue

---
+++ Scripts/Helpers/PropertyReference.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace SRF.Helpers
+{
+
+	public class PropertyReference
+	{
+
+		private object _target;
+		private PropertyInfo _property;
+
+		public string PropertyName { get { return _property.Name; } }
+		public Type PropertyType { get { return _property.PropertyType; } } 
+
+		public PropertyReference(object target, PropertyInfo property)
+		{
+
+			SRDebug.AssertNotNull(target);
+
+			_target = target;
+			_property = property;
+
+		}
+
+		public object GetValue()
+		{
+			return _property.GetValue(_target, null);
+		}
+
+		public void SetValue(object value)
+		{
+			_property.SetValue(_target, value, null);
+		}
+
+	}
+
+}
