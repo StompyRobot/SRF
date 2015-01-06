92f0ac312c2e150f3bc6a2512f53eccbcb616f9f
Simon Moles <simon@stompyrobot.uk>
2015-01-06 00:08:51 +0000

    Added MethodReference helper to match PropertyReference

---
+++ Scripts/Helpers/MethodReference.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace SRF.Helpers
+{
+
+	public class MethodReference
+	{
+
+		private object _target;
+		private MethodInfo _method;
+
+		public string MethodName { get { return _method.Name; } }
+
+		public MethodReference(object target, MethodInfo method)
+		{
+
+			SRDebugUtil.AssertNotNull(target);
+
+			_target = target;
+			_method = method;
+
+		}
+
+		public object Invoke(object[] parameters)
+		{
+
+			return _method.Invoke(_target, parameters);
+
+		}
+
+	}
+
+}
