680fe40fbbe4e71828db57b0e3b4e9df324f0efc
Simon Moles <simon@stompyrobot.uk>
2021-01-01 14:15:32 +0000

    Remove MethodName from MethodReference. Add implicit conversion from Action to MethodReference.

--- Scripts/Helpers/MethodReference.cs
+++ Scripts/Helpers/MethodReference.cs
@@ -6,27 +6,32 @@ namespace SRF.Helpers
 
     public sealed class MethodReference
     {
-        public string MethodName { get; private set; }
-
         private readonly Func<object[], object> _method;
 
         public MethodReference(object target, MethodInfo method)
         {
             SRDebugUtil.AssertNotNull(target);
 
-            MethodName = method.Name;
             _method = o => method.Invoke(target, o);
         }
 
-        public MethodReference(string methodName, Func<object[], object> method)
+        public MethodReference(Func<object[], object> method)
         {
-            MethodName = methodName;
             _method = method;
         }
 
         public object Invoke(object[] parameters)
         {
             return _method.Invoke(parameters);
+        }
+
+        public static implicit operator MethodReference(Action action)
+        {
+            return new MethodReference(args =>
+            {
+                action();
+                return null;
+            });
         }
     }
 }
